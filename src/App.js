64caf5652f1e39deeb99e9ddb094b7e99a57501c
Zaid_ZiD <zid6177@gmail.com>
2022-10-26 13:20:14 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -30,22 +30,16 @@ export default function App() {
   let [classID, setClassID] = useState("")
   const [ip, setIP] = useState('');
 
- 
-
-    //creating function to load ip address from the API
-    const getData = async () => {
-      const res = await axios.get('https://geolocation-db.com/json/')
-      console.log(res.data);
-      setIP(res.data.IPv4)
-    }
-
-  
-
-
-
   useEffect(() => {
     
     let unsubscribe = null;
+
+      //creating function to load ip address from the API
+      const getData = async () => {
+        const res = await axios.get('https://geolocation-db.com/json/')
+        console.log(res.data);
+        setIP(res.data.IPv4)
+      }
 
     getData();
 
