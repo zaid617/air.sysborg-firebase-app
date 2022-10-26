3196b44a0194737d56ac03eb632591fa3df5fed0
Zaid_ZiD <zid6177@gmail.com>
2022-10-26 13:49:16 +0500

    changes in ui

--- src/components/inputSection/Input.jsx
+++ src/components/inputSection/Input.jsx
@@ -3,53 +3,54 @@ import "./Input.css"
 
 export default function Input(props) {
 
-    
+
 
 
     return (
         <div className="container">
 
-            
-      <hr/>
 
-        <form className='input' onSubmit={props.idSub}>
-            <div className="inputs"><input type="text" onChange={(e)=>{props.setClassID(e.target.value)}} value={props.classID} placeholder='Class ID' className='height first-input' /><button type="submit"></button></div>
+            <hr />
 
-            <div className="inputs second">
+            <form className='input' onSubmit={props.idSub}>
+                <div className="inputs"><input type="text" onChange={(e) => { props.setClassID(e.target.value) }} value={props.classID} placeholder='Class ID' className='height first-input' /><button type="submit" className="classBtn">
+                    &#10003;</button></div>
 
-                <input type="text"  placeholder='Enter Any Text Or Link' onChange={(e)=>{props.setText(e.target.value)}} value={props.text} className='height Assignment' />
+                <div className="inputs second">
 
-            <button className='height btn btn-primary m-l3' onClick={props.submitHandler}>Submit</button>
-            </div>
+                    <input type="text" placeholder='Enter Any Text Or Link' onChange={(e) => { props.setText(e.target.value) }} value={props.text} className='height Assignment' />
 
-            <div className="inputs">
-                <button className='height btn btn-primary last' onClick={props.deleteHandler}>Delete all</button>
-            </div>
-        </form>
+                    <button className='height btn btn-primary m-l3' onClick={props.submitHandler}>Submit</button>
+                </div>
+
+                <div className="inputs">
+                    <button className='height btn btn-primary last' onClick={props.deleteHandler}>Delete all</button>
+                </div>
+            </form>
 
 
-        <hr />
+            <hr />
 
-        <p className="error" id="error">Enter Your Class Id !</p>
+            <p className="error" id="error">Enter Your Class Id !</p>
 
-    {(props.textArr.length===0)? null :(
-        <div className="mainBox">
+            {(props.textArr.length === 0) ? null : (
+                <div className="mainBox">
 
-        {
-            props.textArr.map((elem,i)=>{
-             return <MainSection 
-                key={i}
-                id={i}
-                text={elem.assignment}
-                date = {elem.date}
-                ip={elem.ip}
-                />
-            })
-        }
+                    {
+                        props.textArr.map((elem, i) => {
+                            return <MainSection
+                                key={i}
+                                id={i}
+                                text={elem.assignment}
+                                date={elem.date}
+                                ip={elem.ip}
+                            />
+                        })
+                    }
 
-        </div>)
+                </div>)
 
-    }
+            }
 
         </div>
     )
